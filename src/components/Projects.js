5eaeb2f9df871d7c083f89318968aa3c9eb0db98
muteeburrehman <muteeb285@gmail.com>
2024-08-05 14:32:19 +0500

    updated functionalities

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -92,7 +92,7 @@ export const Projects = () => {
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-right" src={colorSharp2}></img>
+            <img className="background-image-right" alt="colorSharpImg" src={colorSharp2}></img>
         </section>
     )
 }
